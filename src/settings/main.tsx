75ad20ffbbf7df0a9eda93a7b5a2cf8285fa729d
jigglypop <52653682+jigglypop@users.noreply.github.com>
2025-07-18 15:48:06 +0900

    0708

--- src/settings/main.tsx
+++ src/settings/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/queryClient'
-import './Settings.module.css'
 import '../font.module.css'
 import Settings from '../containers/settings'
 
